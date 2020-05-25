b62963e4b4c95a490b72b20357df356ef8b89680
YeomChaeEun <yce0222@gmail.com>
2020-05-25 17:46:34 +0900

    전반적인 수정 및 menu img file 정리

--- js/src/headandtop.js
+++ js/src/headandtop.js
@@ -29,11 +29,13 @@
       var winScroll = $(window).scrollTop();
       // console.log(winScroll);
       if(winScroll<=150){
+        headBox.height('240px');
         h1.css({display:'block'});
         pullLogo.css({display:'none'});
         gnbMenu.css({float:'none', margin:'auto', marginTop: '20px'});
         unb.css({top:0});
       }else{
+        headBox.height('90px');
         h1.css({display:'none'});
         pullLogo.css({display:'block'});
         gnbMenu.css({float:'right', margin:0, marginTop: '20px', marginRight:'5%'});
